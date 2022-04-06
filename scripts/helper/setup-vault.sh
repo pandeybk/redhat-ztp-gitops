fbe3e8a8e1865e02cb0f992ccef7bc3d18efc2b0
Balkrishna Pandey <bpandey@redhat.com>
2022-04-06 08:23:58 -0600

    setup vault create vault-token for external secrets operator
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- scripts/helper/setup-vault.sh
+++ scripts/helper/setup-vault.sh
@@ -5,14 +5,13 @@ oc exec -ti vault-server-1 -- vault operator unseal
 oc exec -ti vault-server-2 -- vault operator raft join http://vault-server-0.vault-server-internal:8200
 oc exec -ti vault-server-2 -- vault operator unseal
 
-
 oc exec -ti vault-server-0 -- vault login
 oc exec -ti vault-server-0 -- vault operator raft list-peers
 
+oc exec -ti vault-server-0 -- vault secrets enable -path=secret/ kv
+oc exec -ti vault-server-0 -- vault kv put secret/openshiftpullsecret dockerconfigjson='{"auths":{"cloud.openshift.com":{"auth":"3BlbnNoaWZ0LXJl==","email":"example@redhat.com"},"quay.io":{"auth":"ZZMVhJRUJUR1I3WUwxN05VMQ==","email":"example@redhat.com"},"registry.connect.redhat.com":{"auth":"3BlbnNoaWZ0LXJl==","email":"example@redhat.com"},"registry.redhat.io":{"auth":"==","email":"example@redhat.com"}}}'
 
-vault secrets enable -path=secret/ kv
-vault kv put secret/openshiftpullsecret dockerconfigjson='{"auths":{"cloud.openshift.com":{"auth":"3BlbnNoaWZ0LXJl==","email":"example@redhat.com"},"quay.io":{"auth":"ZZMVhJRUJUR1I3WUwxN05VMQ==","email":"example@redhat.com"},"registry.connect.redhat.com":{"auth":"3BlbnNoaWZ0LXJl==","email":"example@redhat.com"},"registry.redhat.io":{"auth":"==","email":"example@redhat.com"}}}'
-
+oc create secret generic vault-token --from-literal=token="s.STiv0eifCCcX3OoApPvqHJmF"
 
 # Unseal Key 1: +oDOtObq/w3DbzaOZdbD1bOJqE+Dr5ke4ulERAFW5xj2
 # Unseal Key 2: IFm1dcwmgOkX1iSbzwgwvOqxEfVkeNLuUDywXsYW+F3J
