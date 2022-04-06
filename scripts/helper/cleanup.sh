3cfb2d0aec81ce37a5a5f8385405e4cd85a55712
Balkrishna Pandey <bpandey@redhat.com>
2022-04-06 14:39:12 -0600

    cleanup all applications
    Signed-off-by: Balkrishna Pandey <bpandey@redhat.com>

--- scripts/helper/cleanup.sh
+++ scripts/helper/cleanup.sh
@@ -1,1 +1,4 @@
+oc api-resources --verbs=list --namespaced -o name | xargs -n 1 oc get --show-kind --ignore-not-found -n   openshift-gitops
+oc delete application --all -n openshift-gitops
+oc get application --no-headers -n openshift-gitops  -o name | xargs -n 1 oc patch -n openshift-gitops -p '{"metadata":{"finalizers":null}}' --type=merge
 oc patch argocd openshift-gitops -n openshift-gitops -p '{"metadata":{"finalizers":null}}' --type=merge
