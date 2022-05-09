c309471d65cb0f6b59f3ba05177d7c3c3152ab3c
sachit.nep@gmail.com <bpandey@redhat.com>
2022-05-09 12:55:23 -0600

    create secrets
    Signed-off-by: sachit.nep@gmail.com <bpandey@redhat.com>

--- scripts/helper/setup-vault.sh
+++ scripts/helper/setup-vault.sh
@@ -11,7 +11,7 @@ oc exec -ti vault-server-0 -- vault operator raft list-peers
 oc exec -ti vault-server-0 -- vault secrets enable -path=secret/ kv
 oc exec -ti vault-server-0 -- vault kv put secret/openshiftpullsecret dockerconfigjson='{"auths":{"cloud.openshift.com":{"auth":"3BlbnNoaWZ0LXJl==","email":"example@redhat.com"},"quay.io":{"auth":"ZZMVhJRUJUR1I3WUwxN05VMQ==","email":"example@redhat.com"},"registry.connect.redhat.com":{"auth":"3BlbnNoaWZ0LXJl==","email":"example@redhat.com"},"registry.redhat.io":{"auth":"==","email":"example@redhat.com"}}}'
 
-oc create secret generic vault-token --from-literal=token="s.STiv0eifCCcX3OoApPvqHJmF"
+oc create secret generic vault-token --from-literal=token="s.68yvrsufCKn5gO7kIxoJFLl3" -n vault 
 
 # Unseal Key 1: vDOUH+64ClDDMoUETLOwM0t25jlsSlF4Gp9Uo50VazIY
 # Unseal Key 2: BTVywAKZB3+cBFaCiaD6ETGo8OqCD3mYo7nDBDEtgocW
