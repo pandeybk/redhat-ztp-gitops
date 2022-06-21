26bc2e38a95e3d9e41353430009f4ba39872b728
Balkrishna Pandey <sachit.nep@gmail.com>
2022-06-21 08:54:02 -0600

    vault token
    Signed-off-by: Balkrishna Pandey <sachit.nep@gmail.com>

--- scripts/helper/setup-vault.sh
+++ scripts/helper/setup-vault.sh
@@ -11,12 +11,12 @@ oc exec -ti vault-server-0 -- vault operator raft list-peers
 oc exec -ti vault-server-0 -- vault secrets enable -path=secret/ kv
 oc exec -ti vault-server-0 -- vault kv put secret/openshiftpullsecret dockerconfigjson='{"auths":{"cloud.openshift.com":{"auth":"3BlbnNoaWZ0LXJl==","email":"example@redhat.com"},"quay.io":{"auth":"ZZMVhJRUJUR1I3WUwxN05VMQ==","email":"example@redhat.com"},"registry.connect.redhat.com":{"auth":"3BlbnNoaWZ0LXJl==","email":"example@redhat.com"},"registry.redhat.io":{"auth":"==","email":"example@redhat.com"}}}'
 
-oc create secret generic vault-token --from-literal=token="s.68yvrsufCKn5gO7kIxoJFLl3" -n vault 
+oc create secret generic vault-token --from-literal=token="s.XhitRtJcA8CVT7a9xfK804KT" -n vault 
 
-# Unseal Key 1: vDOUH+64ClDDMoUETLOwM0t25jlsSlF4Gp9Uo50VazIY
-# Unseal Key 2: BTVywAKZB3+cBFaCiaD6ETGo8OqCD3mYo7nDBDEtgocW
-# Unseal Key 3: q5AyTKv7+Lm4SSAz2vguZxD+bSy+YD85CPcLwFASGJoc
-# Unseal Key 4: 4yXKKTYPiigjnf5QAoEnutkqOlEDeTfc7E3rtX/8EZ8f
-# Unseal Key 5: dd2q6sLTKCvrupmAiySWsjCKmackWZeQgrKy1X0rkV13
+Unseal Key 1: w/wo0NYvY4wYyykUOX/ankxBm3mrIoEnnrFPivZyoo33
+Unseal Key 2: CgF455nzVmq4i0BVhZdZ3O8FiO0p+yBxpNBCJSfOoy8p
+Unseal Key 3: UufT91fbNjDBlMrpIQSO66B2zJUg0KoriDuFJ+U44SPi
+Unseal Key 4: KpNJ/3eMSQZYnmiBPr3Hdre9BOYaLfPetVpRFNUYOS9U
+Unseal Key 5: 0FHMN/kjLgQY8ljhN+9xS5I4ZS3zG1vBYJYi9L5+/gWY
 
-# Initial Root Token: s.68yvrsufCKn5gO7kIxoJFLl3
+Initial Root Token: s.XhitRtJcA8CVT7a9xfK804KT
